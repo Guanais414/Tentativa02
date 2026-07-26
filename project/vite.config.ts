8e494bc7d2a5727c58f34f5c2d5f9a602ed89c25
Guanais414 <joaopedroguanais@gmail.com>
2026-07-26 08:16:34 -0300

    Update vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -4,8 +4,8 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
- base: '/Tentativa02/'
   plugins: [react()],
+ base: './'
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
