439e9380b3070f288d41b2bc96fc53fde5e7a648
Guanais414 <joaopedroguanais@gmail.com>
2026-07-26 09:18:35 -0300

    Update vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
- base: '/'
+  base: '/', 
+})
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
